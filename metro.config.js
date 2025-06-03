83235333e1734cccb1d21cea251de0b19a034b09
Gabriel <ovallegabriel123@gmail.com>
2025-06-03 00:51:33 -0400

    feat: trying to make the production development work properly

--- metro.config.js
+++ metro.config.js
@@ -3,16 +3,17 @@ const { getDefaultConfig } = require('@expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.extraNodeModules = {
+  ...defaultConfig.resolver.extraNodeModules,
   events: require.resolve('events/'),
   http: require.resolve('stream-http'),
   https: require.resolve('https-browserify'),
   net: require.resolve('node-libs-browser/mock/net'),
   url: require.resolve('url/'),
+  fs: require.resolve('expo-file-system'),
   stream: require.resolve('stream-browserify'),
   crypto: require.resolve('crypto-browserify'),
   tls: require.resolve('node-libs-browser/mock/tls'),
   zlib: require.resolve('browserify-zlib'),
-  fs: require.resolve('node-libs-browser/mock/empty'),
   path: require.resolve('path-browserify'),
   os: require.resolve('os-browserify/browser'),
 };
